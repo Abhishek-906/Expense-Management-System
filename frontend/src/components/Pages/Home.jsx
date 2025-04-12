4147b8af4d7adb972fc11be9ae8e1222bb3eef47
abhi-906 <87569322+abhi-906@users.noreply.github.com>
2025-04-12 23:08:44 +0530

    changes

--- frontend/src/components/Pages/Home.jsx
+++ frontend/src/components/Pages/Home.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "../../index.css";
 import SetBudget from "./SetBudget";
 import DashboardPart from "./dashbordpart";
-import Transaction from "./Transaction";
+import Transaction from "./transaction";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const Home = () => {
