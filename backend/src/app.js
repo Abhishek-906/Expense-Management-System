4a36e6307feabcb12d8a346fda8f98c922d332fb
abhi-906 <87569322+abhi-906@users.noreply.github.com>
2025-04-13 15:03:33 +0530

    deploy-error 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,25 +6,7 @@ const mongoose = require('mongoose');
 const cors = require("cors");
 const app = express();
 
-
-// Allow both localhost and production frontend
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://expense-management-system-50fmjuoga-abhishks-projects.vercel.app'
-];
-
-console.log("allowedOrigins ",allowedOrigins)
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(cors({ origin: "*" }));
 
 
 app.use(express.json());
@@ -34,6 +16,5 @@ connectDB();
 
 app.use('/user', require('./Routes/userRoute'));
 app.use('/transaction', require('./Routes/transactionRoute'));
-
 
 module.exports = app;
